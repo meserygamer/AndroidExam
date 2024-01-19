3b81b4f4fb7239d59564401e4812e55e82f05730
MeseryGG <Meserygamer@gmail.com>
2024-01-20 00:37:55 +0300

    Присоединен биндинг для страницы авторизации

--- app/src/main/java/com/example/androidexam/AuthActivity.kt
+++ app/src/main/java/com/example/androidexam/AuthActivity.kt
@@ -2,10 +2,17 @@ package com.example.androidexam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidexam.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+
+    var _binding : ActivityAuthBinding? = null; //Биндинг активности
+    val binding : ActivityAuthBinding //Свойство для доступа к биндингу без постоянного прописывания _binding!!
+        get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        _binding = ActivityAuthBinding.inflate(layoutInflater); //Создаём биндинг
+        setContentView(binding.root) //Открываем страницу на основе созданного биндинга
     }
 }
